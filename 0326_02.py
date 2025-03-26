1763171e45a3313509e2dbf81903863568de100f
고서연 <sykoh1205@naver.com>
2025-03-26 15:46:17 +0900

    feat: 0326파이썬실습

---
+++ 0326_02.py
@@ -0,0 +1,22 @@
+import datetime
+
+now = datetime.datetime.now()
+month = now.month
+if 3 <= month <= 5:
+    print('현재는 봄입니다')
+elif 6 <= month <= 8:
+    print('현재는 여름입니다')
+elif 9 <= month <= 11:
+    print('현재는 가을입니다')
+else:
+    print('현재는 겨울입니다다')
+
+
+x = 10
+y = 2
+
+if x > 4:
+    if y > 2:
+        print(x*y)
+else:
+    print(x+y)
